769749f79ec35ec8d8759eef3e4434cc8b4a62d3
sunnanv <dat15jsu@student.lu.se>
2017-11-11 23:57:57 +0100

    Working on dictionary

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -9,22 +9,46 @@
 using std::string;
 using std::vector;
 
-void add_trigram_suggestions(vector<string>& suggestions, const string& missWord)
+void get_trigrams(vector<string>& trigrams, const string& word)
 {
-	int size = missWord.size();
-	int trigrams = size-2;
-	if(trigrams > 1)
+	if(word.size()-2 > 0)
 	{
-		for(Word word : words)
+		for(int i = 0; i != word.size()-2; ++i)
 		{
-			if(word.get_matches(missWord) >= trigrams/2)
-			{
-				suggestions.push_back(word.get_word());
-			}
+			trigrams.push_back(word.substr(i, 3));
 		}
 	}
-	
+} 
+
+void Dictionary::rank_suggestions(vector<string>& suggestions, const string& missWord) const
+{
+
 }
+
+void Dictionary::add_trigram_suggestions(vector<string>& suggestions, const string& missWord) const
+{
+	int size = missWord.size();
+	vector<string> trigrams;
+	//vector<Word::Word> relevantWords(Dictionary::words[size-1]);
+	//relevantWords.insert(relevantWords.end(), Dictionary::words[size].begin(), Dictionary::words[size].end());
+	//relevantWords.insert(relevantWords.end(), Dictionary::words[size+1].begin(), Dictionary::words[size+1].end());
+	get_trigrams(trigrams, missWord);
+	if(trigrams.size() != 0)
+	{
+		for(int i = -1; i != 2; ++i)
+		{
+			for(Word word : Dictionary::words[size+i])
+			{
+				if(word.get_matches(trigrams) >= trigrams.size() / 2) {
+					suggestions.push_back(word.get_word());
+					std::cout << word.get_word() << std::endl;	
+				}
+			}
+		}
+		
+	}
+	
+} 
 
 Dictionary::Dictionary() {
 	std::ifstream in("words.txt");
@@ -60,6 +84,7 @@ Dictionary::Dictionary() {
 			Dictionary::word_set.insert(str);
 		}
 	}
+	
 }
 
 bool Dictionary::contains(const string& word) const {
@@ -69,11 +94,19 @@ bool Dictionary::contains(const string& word) const {
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
 	vector<string> suggestions;
-	add_trigram_suggestions(suggestions, word);
+	Dictionary::add_trigram_suggestions(suggestions, word);
+	Dictionary::rank_suggestions(suggestions, word);
 	return suggestions;
 }
+
+
 
 int main()
 {
 	Dictionary dict;
+	vector<string> vect(dict.get_suggestions("abbatil"));
+	for(string s:vect)
+	{
+		std::cout << s << std::endl;
+	}
 }
--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -11,6 +11,9 @@ public:
 	Dictionary();
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
+	void add_trigram_suggestions(std::vector<std::string>& suggestions, const std::string& missWord) const;
+	void rank_suggestions(std::vector<std::string>& suggestions, const std::string& missWord) const;
+	//std::vector<Word::Word> get_words(){return words;};
 private:
 	std::unordered_set<std::string> word_set;
 	std::vector<Word::Word> words[25];
