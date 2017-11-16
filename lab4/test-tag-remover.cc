3c3a55c4a03f3b39a940158612b41637edbf2e58
sunnanv <dat15jsu@student.lu.se>
2017-11-16 22:20:26 +0100

    lab3 a0-1

--- lab4/test-tag-remover.cc
+++ lab4/test-tag-remover.cc
@@ -5,13 +5,13 @@
 
 using std::string;
 
-void test(string fname)
+bool test(string fname)
 {
 	TagRemover tr("files/" + fname + ".html");
 	tr.print("files/" + fname + "_result.txt");
 
 	std::ifstream f1{"files/" + fname + "_correct.txt"};
-	std::ifstream f2{"files/" + fname + "_correct.txt"};
+	std::ifstream f2{"files/" + fname + "_result.txt"};
 
 	char c1, c2;
 	while((c1 = f1.get()) != EOF && (c2 = f2.get()) != EOF)
@@ -19,17 +19,32 @@ void test(string fname)
 		if(c1 != c2)
 		{
 			std::cout << fname << ": files not equal" << std::endl;
-			return;
+			return false;;
 		}
 	}
-	if((f1.eof() && !f2.eof()) || (!f1.eof() && f2.eof()))
-			std::cout << fname << ": files not equal" << std::endl;
+	if((c1 = f1.peek()) != EOF || (c2 = f2.peek()) != EOF)
+	{
+		std::cout << fname << ": files not equal" << std::endl;
+		return false;
+
+	}
+	std::cout << fname << ": PASSED" << std::endl;
+	return true;
 }
 int main()
 {
-	test("test_tag_remove");
-	test("test_spchar_replace");
-	test("test_double_tag_remove");
-	test("test_multiline_remove_tag");
-	test("test");
+
+	std::cout << "== test-tag-remover ===============" << std::endl;
+	bool passed{true};
+	bool a = test("test_tag_remove");
+	bool b = test("test_spchar_replace");
+	bool c = test("test_double_tag_remove");
+	bool d = test("test_multiline_remove_tag");
+	bool e = test("test");
+
+	passed = a && b && c && d && e;
+	if(passed) {
+		std::cout << "-- All tests passed! --------------" << std::endl;
+	}
+	std::cout << "===================================" << std::endl;
 }
